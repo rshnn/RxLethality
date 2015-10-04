dd6741f24e47f21c8994f35e69ff7e347ebb0aa9
Roshan Patel <roshanpatel06@gmail.com>
2015-10-04 05:41:13 -0400

    Why wont the bootstrap work

--- app.js
+++ app.js
@@ -1,51 +1,66 @@
-var express = require('express');
-var app = express();
-var request = require('request');
+var express     = require('express');
+var request     = require('request');
+var bodyParser  = require('body-parser');
 
 
-// Visit localhost:3000/
-app.get('/fda', function(req, res) {
-  // Pull the search term out of the query argument
-  var search = req.query.q;
-  
-  // Make the giphy api request passing in our search term
-  request('https://api.fda.gov/drug/event.json?api_key=CNoTQXXfsG6hmF8EzbvS0Z8OcOiZNEtdmGm2OGZA&search=' + search, function(err, resp, body) {
-    // Parse the response body as json
-    var json = JSON.parse(body);
-    
-    res.send(json);
+
+var app         = express();
+
+app.use(bodyParser.urlencoded({ extended: true }))
+app.use(bodyParser.json())
+
+
+app.set('view engine', 'jade');
+app.set('views', './views/');
+
+
+
+app.get('/searchinit', function(req, res){
+  res.render('index-form');
+})
+
+app.post('/searching', function(req,res){
+
+  var search = req.body.drug;
+  console.log(search);
+  //res.send('Searching for ' + search + ' in database...');
+
+  request('https://api.fda.gov/drug/event.json?api_key=CNoTQXXfsG6hmF8EzbvS0Z8OcOiZNEtdmGm2OGZA&search=patient.drug.medicinalproduct:' + search, function(err, resp, body) {
+
+    var json            = JSON.parse(body);
+    var searchlength    = json.meta.results.total;
+
+    if(searchlength >= 100){
+      searchlength = 20;
+    }
+
+
+    request('https://api.fda.gov/drug/event.json?api_key=CNoTQXXfsG6hmF8EzbvS0Z8OcOiZNEtdmGm2OGZA&limit='+searchlength+'&search=patient.drug.medicinalproduct:' + search, function(err, resp, body) {
+      var json = JSON.parse(body);
+
+
+      var i     = 0;
+      var count = 0;
+      while(i < searchlength){
+
+        var serious = json.results[i].serious;
+        if(serious == 1){
+          count++;
+        }
+        i++;
+      };
+
+      res.render('index', { ratio: 100*count/searchlength, drugname: search });
+
+
+    });
   });
-});
 
-
-app.get('/', function(req, res) {
-  // Pull the search term out of the query argument
-
-
-  // Make the giphy api request passing in our search term
-  request('https://api.fda.gov/drug/event.json?api_key=CNoTQXXfsG6hmF8EzbvS0Z8OcOiZNEtdmGm2OGZA&search="death"', function(err, resp, body) {
-    // Parse the response body as json
-    var json = JSON.parse(body);
-
-    var drug = json.results[1].patient;
-    console.log(drug);
-    
-
-   	res.send(drug);
-  });
-});
-
-//log.typeof();
-// npm install
-// res.render
+})
 
 
 
-
-app.get('/:name', function(req, res) {
-  var name = req.params.name;
-  res.send('Hello, ' + name);
-});
 
 
 app.listen(3000);
+console.log("listening");
